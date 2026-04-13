5fde4e12a7dc31cfd3687bc34817b03e66ebb2c3
Julio Santa Cruz <bartacruz@gmail.com>
2026-04-13 04:06:48 -0300

    [IMP] force_developer: added license to manifest

--- force_developer_mode/__manifest__.py
+++ force_developer_mode/__manifest__.py
@@ -10,7 +10,7 @@ Force developer mode if you are an admin
 
     'author': "Bartatech",
     'website': "https://github.com/bartacruz/bartatech-odoo",
-
+    "license": "AGPL-3",
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
